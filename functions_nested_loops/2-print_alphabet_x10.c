f9773966b8f0e262caf109b1167417655779a737
root <manuelfran12@hotmail.com>
2022-06-17 14:00:02 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
-* print_alphabet - print alphabet
+* print_alphabet_x10 - print alphabet x10
 * Return: 0
 */
 void print_alphabet_x10(void)
 {
-
-int n_repe;
 char alp;
-
-for (n_repe = '0'; n_repe <= '9'; n_repe++)
+int n_repe;
+for (n_repe = 0; n_repe <= 9; n_repe++)
 {
-for (alp  = 'a'; alp <= 'z'; alp++)
+for (alp = 'a'; alp <= 'z'; alp++)
 {
-_putchar(alp);
+_putchar (alp);
 }
-_putchar('\n');
+_putchar ('\n');
 }
 }
