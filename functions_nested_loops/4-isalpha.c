c03ad51b152c75ffca3538389e6addb04766c9a3
root <manuelfran12@hotmail.com>
2022-06-17 16:25:35 -0700

    Write a function that checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - check alphabet character
+ * @c: check
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+char ref;
+char REF;
+for (ref = 'a'; ref <= 'z'; ref++)
+{
+if (c  == ref)
+return (1);
+}
+for (REF = 'A'; REF <= 'Z'; REF++)
+{
+if (c == ref)
+return (1);
+}
+return (0);
+}
